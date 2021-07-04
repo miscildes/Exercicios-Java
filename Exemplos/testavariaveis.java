fefa4098dd1c2ee8a43df61b856b54bb5288fc7b
Guilherme (Gillie) <miscildes@gmail.com>
2021-07-03 22:21:00 -0300

    commit exemplos

---
+++ Exemplos/testavariaveis.java
@@ -0,0 +1,18 @@
+
+public class testavariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("ola novo teste");
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println("A sua idade é " + idade);
+		
+		double notas;
+		notas = 7.5;
+				
+		System.out.println("A sua nota é " + notas);
+	}
+
+}
